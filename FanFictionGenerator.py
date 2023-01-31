7afda5f9e665e315bb09e5e0412e6728be3588ec
thepok <richterthepok@yahoo.de>
2023-01-31 15:01:40 +0100

    Merge two stories to improved one

--- FanFictionGenerator.py
+++ FanFictionGenerator.py
@@ -57,30 +57,68 @@ Now Rewrite the Story with the new Details from the Solution.
 New Version:""",
         )
 
+        self.StoryMerger = LLMChain.from_string(self.llm, """Two stories for {seriesname} have to be merged.
+Story 1:{plot1}
+
+Story 2:{plot2}
+
+Your task is it to merge this two stories into one more exciting story.
+
+Merged Story:""")
+
+    def merge(self, seriesname, plot1, plot2):
+        print("Merging two Stories")
+        merged = self.StoryMerger.predict(
+            seriesname=seriesname, plot1=plot1, plot2=plot2)
+        print("Merged Plot: " + merged)
+        return merged
 
     def generateInitial(self, seriesname, UseCache=True):
         print("Generating a new FanFiction")
-        self.llm.cache=UseCache
+        self.llm.cache = UseCache
         plot = self.initialPlotGenerator.predict(seriesname=seriesname)
         print("Generated Plot: " + plot)
         
         return plot
 
-    def improve(self, seriesname, plot):
-        problems = self.problemGenerator.predict(
-            plot=plot,
-            seriesname=seriesname)
-        print("Generated Problems:" + problems)
+    def improve(self, seriesname, plot, count=1):
 
-        solution = self.ProblemFixer.predict(
-            plot=plot, seriesname=seriesname, problems=problems
-        )
-        print("Generated Solution: " + solution)
+        for i in range(count):
+            problems = self.problemGenerator.predict(
+                plot=plot,
+                seriesname=seriesname)
+            print("Generated Problems:" + problems)
 
-        nextVersion = self.NextVersion.predict(
-            plot=plot, problems=problems,
-            solution=solution,
-            seriesname=seriesname
-        )
+            solution = self.ProblemFixer.predict(
+                plot=plot, seriesname=seriesname, problems=problems
+            )
+            print("Generated Solution: " + solution)
+
+            plot = self.NextVersion.predict(
+                plot=plot, problems=problems,
+                solution=solution,
+                seriesname=seriesname
+            )
+        return plot
+
+    def MultiGenerate(
+            self, seriesname,
+            improvementSteps=3,
+            storyCount=2,
+            finalImprovementSteps=3):
+        plots = []
+        for i in range(storyCount):
+            plot = self.generateInitial(seriesname, False)
+            plot = self.improve(seriesname, plot, improvementSteps)
+            print("Generated Plot" + str(i) + ": " + plot)
+            plots.append(plot)
         
-        return nextVersion
+        mergedPlot = plots[0]
+        for i in range(1, len(plots)):
+            mergedPlot = self.merge(seriesname, mergedPlot, plots[i])
+
+        # finalImprovement
+        mergedPlot = self.improve(
+            seriesname, mergedPlot, finalImprovementSteps)
+        
+        return mergedPlot
